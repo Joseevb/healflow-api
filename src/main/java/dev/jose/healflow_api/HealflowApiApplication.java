8b3246d4735b1dee08df94134bb61d82f7c887d9
Jose Vasquez <joseevb@protonmail.com>
2026-02-22 15:14:20 +0100

    feat: enabled retry

--- src/main/java/dev/jose/healflow_api/HealflowApiApplication.java
+++ src/main/java/dev/jose/healflow_api/HealflowApiApplication.java
@@ -3,8 +3,10 @@ package dev.jose.healflow_api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Slf4j
+@EnableRetry
 @SpringBootApplication
 public class HealflowApiApplication {
 
